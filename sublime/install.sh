b0e4a7c8e453de3c0e1063d3a43a611fe489aa1a
Ryan Olson <ryanolsonx@gmail.com>
2018-04-04 08:21:57 -0600

    Latest

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,1 +1,2 @@
 ln -s ~/dotfiles/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
