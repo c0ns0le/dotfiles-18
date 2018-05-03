dc9eda38c5fb279744cbe4e0a40f9e17c4352588
Ryan Olson <ryan.olson@gartner.com>
2018-05-03 13:08:50 -0600

    Latest to start fresh with literate programming

--- emacs/elpa/doneburn-theme-20180418.47/doneburn-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "doneburn-theme" "20180418.47" "A light theme based on Bozhidar Batsov's Zenburn" 'nil :commit "248f52070b076ca93d03aa5d1b0008189a166fa3" :keywords '("faces" "themes") :url "http://github.com/manuel-uberti/doneburn-emacs")
