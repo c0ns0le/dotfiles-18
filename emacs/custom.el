f28cc113d92de8cc8b9b3f2fd567a3f575ec7b73
Ryan Olson <ryan.olson@gartner.com>
2018-05-03 23:49:08 -0600

    Install and switch to solarized

--- emacs/custom.el
+++ emacs/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (delight diminish counsel-projectile projectile elm-mode markdown-mode elpy add-node-modules-path color-theme-sanityinc-tomorrow counsel editorconfig engine-mode expand-region ivy less-css-mode magit nlinum npm-mode prettier-js rainbow-mode swiper tide which-key yasnippet yasnippet-snippets company))))
+    (solarized-theme delight diminish counsel-projectile projectile elm-mode markdown-mode elpy add-node-modules-path color-theme-sanityinc-tomorrow counsel editorconfig engine-mode expand-region ivy less-css-mode magit nlinum npm-mode prettier-js rainbow-mode swiper tide which-key yasnippet yasnippet-snippets company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
